75380e7c379503df52ab556c3e5803242350c617
CreatorBailey <baileym1215@gmail.com>
2019-11-14 16:58:04 -0500

    first commits

---
+++ src/main/java/DuoBank/DuoBank/DuoBankApplication.java
@@ -0,0 +1,13 @@
+package DuoBank.DuoBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuoBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuoBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DuoBank/DuoBank/DuoBankApplicationTests.java
@@ -0,0 +1,13 @@
+package DuoBank.DuoBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuoBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
